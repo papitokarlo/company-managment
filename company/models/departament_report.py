5ca217bdbade40d32a29cc8c3ebcb3cdc3340108
papitokarlo <81024181+papitokarlo@users.noreply.github.com>
2022-03-15 23:16:18 +0400

    cleaned
    cleaned from bugs

--- company/models/departament_report.py
+++ company/models/departament_report.py
@@ -8,21 +8,18 @@ class DepartamentReport(models.AbstractModel):
 
     departament = fields.Char(string="Departament")
     amount = fields.Integer(string="Departament members")
+    manager = fields.Char(string="manager")
     
-    def _select(self):
-        select = f"""
-                    SELECT row_number() OVER () AS id, employeer_departament.departament AS departament,  COUNT(company_employee.departament) AS amount
-                    FROM employeer_departament 
-                    LEFT JOIN company_employee ON employeer_departament.id = company_employee.departament
-                    GROUP BY employeer_departament.departament
-                    
-                """
-        print("_____-----------_-------------_-------------__________-___-------_-_____-____-_-_---", select)
-        return select
+    
 
     def init(self):
         tools.drop_view_if_exists(self._cr, 'departament_report_sql')
-        self._cr.execute(f"""
+        self._cr.execute("""
                         CREATE OR REPLACE VIEW departament_report_sql AS (
-                            {self._select()}
+                            SELECT row_number() OVER () AS id, employeer_departament.departament AS departament, 
+                            company_employee.name AS manager, 
+                            COUNT(company_employee.departament) AS amount
+                            FROM employeer_departament 
+                            LEFT JOIN company_employee ON employeer_departament.id = company_employee.departament
+                            GROUP BY employeer_departament.departament, company_employee.name
                         )""")
